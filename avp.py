2d0f7d033b7b91abf2bdbab02ff66a9a22e67c7e
kAif-gh <80365885+kAif-gh@users.noreply.github.com>
2022-11-21 23:30:20 +0100

    python image to DockerHub

---
+++ avp.py
@@ -0,0 +1,27 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+    'start': '36',
+    'limit': '1',
+
+
+}
+headers = {
+    'Accepts': 'application/json',
+    # 'X-CMC_PRO_API_KEY': '140b2cf8-1299-4718-8e0b-886f9c46dde0',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+    json_string = json.dumps(data['data'])
+
+    print(json_string)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+    print(e)
