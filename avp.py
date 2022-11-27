8b5c860bed08a78a5cab2e325fa5a724e82a3612
kAif-gh <80365885+kAif-gh@users.noreply.github.com>
2022-11-27 23:54:15 +0100

    added .env to create a TOKEN Key

--- avp.py
+++ avp.py
@@ -1,6 +1,8 @@
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
+import os
+TOKEN = os.environ.get("TOKEN_KEY")
 
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 parameters = {
@@ -12,6 +14,7 @@ parameters = {
 headers = {
     'Accepts': 'application/json',
     'X-CMC_PRO_API_KEY': '140b2cf8-1299-4718-8e0b-886f9c46dde0',
+    # 'X-CMC_PRO_API_KEY': TOKEN,
 }
 
 session = Session()
@@ -25,3 +28,39 @@ try:
     print(json_string)
 except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
+
+
+
+
+# from requests import Request, Session
+# from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+# import json
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()  # take environment variables from .env.
+# TOKEN = os.environ.get("TOKEN_KEY")
+#
+# url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+# parameters = {
+#     'start': '36',
+#     'limit': '1',
+#
+#
+# }
+# headers = {
+#     'Accepts': 'application/json',
+#     'X-CMC_PRO_API_KEY': TOKEN,
+# }
+#
+# session = Session()
+# session.headers.update(headers)
+#
+# try:
+#     response = session.get(url, params=parameters)
+#     data = json.loads(response.text)
+#     json_string = json.dumps(data['data'])
+#
+#     print(json_string)
+#     print("hello")
+# except (ConnectionError, Timeout, TooManyRedirects) as e:
+#     print(e)
