6b2cf4d2f94fb7aa0b3cfb8a057a968d324d8085
Ghaith BenHaj <80365885+kAif-gh@users.noreply.github.com>
2022-11-28 00:25:16 +0100

    Update avp.py

--- avp.py
+++ avp.py
@@ -26,41 +26,7 @@ try:
     json_string = json.dumps(data['data'])
 
     print(json_string)
+    print("hello")
 except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
 
-
-
-
-# from requests import Request, Session
-# from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
-# import json
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()  # take environment variables from .env.
-# TOKEN = os.environ.get("TOKEN_KEY")
-#
-# url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-# parameters = {
-#     'start': '36',
-#     'limit': '1',
-#
-#
-# }
-# headers = {
-#     'Accepts': 'application/json',
-#     'X-CMC_PRO_API_KEY': TOKEN,
-# }
-#
-# session = Session()
-# session.headers.update(headers)
-#
-# try:
-#     response = session.get(url, params=parameters)
-#     data = json.loads(response.text)
-#     json_string = json.dumps(data['data'])
-#
-#     print(json_string)
-#     print("hello")
-# except (ConnectionError, Timeout, TooManyRedirects) as e:
-#     print(e)
